b5f73c86cf0767703beabb9f9a282bc24ef9d661
je <jemin@uoregon.edu>
2018-09-28 23:34:09 -0700

    fixed main page

--- static/js/background.js
+++ static/js/background.js
@@ -11,5 +11,5 @@ function showSlides() {
     if (slideIndex > slides.length) {slideIndex = 1}    
 
     slides[slideIndex-1].style.display = "block";  
-    setTimeout(showSlides, 2500);
+    setTimeout(showSlides, 3000);
 }
